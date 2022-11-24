c0521ab24b41d4ac92810ea49614a07db3f980cf
Camille Semaan <csemaan@cmaisonneuve.qc.ca>
2022-11-24 11:59:45 -0500

    Début intégration de la connexion

--- index.php
+++ index.php
@@ -1,16 +1,33 @@
 <?php
     include('inclusions/entete.inc.php');
+
+    if(isset($_POST['courriel'])) {
+        $courriel =  $_POST['courriel'];
+        $mdp = hash('sha3-256', $_POST['mdp']);
+
+        $cnx = mysqli_connect('localhost', 'root', '', 'leila');
+        mysqli_set_charset($cnx, 'utf8');
+        $reponse = mysqli_query($cnx, "SELECT * FROM utilisateur 
+                        WHERE courriel='$courriel' AND mdp='$mdp'");
+        $utilisateur = mysqli_fetch_assoc($reponse);
+        if($utilisateur) {
+            echo "Vous êtes connecté ...";
+        }
+        else {
+            echo "Mauvaise combinaison courriel/mot de passe";
+        }
+    }    
 ?>
     <section class="gestion-utilisateur">
-        <form class="connexion">
+        <form class="connexion" method="POST">
             <legend>Ouvrir une connexion</legend>
             <div class="champs">
-                <label for="uti_courriel">Courriel</label>
-                <input type="email" name="uti_courriel" id="uti_courriel" placeholder="Adresse de courriel">
+                <label for="courriel">Courriel</label>
+                <input type="email" name="courriel" id="courriel" placeholder="Adresse de courriel">
             </div>
             <div class="champs">
-                <label for="uti_mdp">Mot de passe</label>
-                <input type="password" name="uti_mdp" id="uti_mdp" placeholder="Mot de passe">
+                <label for="mdp">Mot de passe</label>
+                <input type="password" name="mdp" id="mdp" placeholder="Mot de passe">
             </div>
             <input class="btn btn-connexion" type="submit" value="Connexion">
         </form>
